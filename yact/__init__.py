dfd615d89ac12ac183fa9eab4cffcd668769f6f0
Jesse Roberts <jesse@clearwateranalytics.com>
2016-10-14 14:36:21 -0600

    docs things

--- yact/__init__.py
+++ yact/__init__.py
@@ -1,1 +1,6 @@
 from .config import Config, from_file, ConfigEditFailed, MissingConfig, InvalidConfigFile
+
+__author__ = 'Jesse Roberts'
+__email__ = 'jesse@hackedpotatoes.com'
+__version__ = '0.3.0'
+
